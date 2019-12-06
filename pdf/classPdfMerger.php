24687daf0fb495888a9b0d618bf7f0554c7619fe
Robin Speekenbrink <robin@kingsquare.nl>
2019-12-06 09:36:33 +0100

    promote the use of a single factory method that also cleans instead of the public stuff

--- pdf/classPdfMerger.php
+++ pdf/classPdfMerger.php
@@ -7,16 +7,56 @@ require_once __DIR__.'/fpdi/fpdi.php';
 
 class PdfMerger extends fpdi {
 
-    var $files = array();
+    /**
+     * @deprecated
+     * @var string[]
+     */
+    var $files = [];
 
-    function PdfMerger($orientation='P',$unit='mm',$format='A4') {
-        parent::fpdi($orientation,$unit,$format);
+    /**
+     * @param string[] $files
+     * @param bool $unlinkTheSourceFilesAfterMerge
+     * @return \PdfMerger
+     */
+    public static function fromFilesIntoSinglePdf(array $files, $unlinkTheSourceFilesAfterMerge = true)
+    {
+        $instance = new self();
+        $instance->files = $files;
+        $instance->merge();
+        if (!$unlinkTheSourceFilesAfterMerge) {
+            return $instance;
+        }
+
+        foreach ($instance->files as $file) {
+            @unlink($file);
+        }
+        $instance->files = [];
+        return $instance;
     }
 
+
+    /**
+     * PdfMerger constructor.
+     * @deprecated The public constructor is deprecated, please use the fromFilesIntoSinglePdf factory method
+     * @param string $orientation
+     * @param string $unit
+     * @param string $format
+     */
+    function __construct($orientation='P',$unit='mm',$format='A4') {
+        parent::__construct($orientation,$unit,$format);
+    }
+
+    /**
+     * @deprecated will be (re)moved  / privatised
+     * @param $files
+     */
     function setFiles($files) {
         $this->files = $files;
     }
 
+    /**
+     * @deprecated will be (re)moved  / privatised
+     */
     function merge() {
         foreach ($this->files AS $file) {
             $pagecount = $this->setSourceFile($file);
@@ -29,4 +69,3 @@ class PdfMerger extends fpdi {
     }
 
 }
-
