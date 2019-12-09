dc8ff7b05e5220975e9fc45ed6311d0a063fcd75
Robin Speekenbrink <robin@kingsquare.nl>
2019-12-09 13:19:45 +0100

    simplified interface into the single static factory

--- pdf/classPdfMerger.php
+++ pdf/classPdfMerger.php
@@ -1,17 +1,13 @@
 <?php
+
 if (!defined('FPDF_FONTPATH')) {
-	define('FPDF_FONTPATH','font/');
+    define('FPDF_FONTPATH', 'font/');
 }
-require_once __DIR__.'/fpdf/fpdf.php';
-require_once __DIR__.'/fpdi/fpdi.php';
+require_once __DIR__ . '/fpdf/fpdf.php';
+require_once __DIR__ . '/fpdi/fpdi.php';
 
-class PdfMerger extends fpdi {
-
-    /**
-     * @deprecated
-     * @var string[]
-     */
-    var $files = [];
+final class PdfMerger extends FPDI
+{
 
     /**
      * @param string[] $files
@@ -21,51 +17,20 @@ class PdfMerger extends fpdi {
     public static function fromFilesIntoSinglePdf(array $files, $unlinkTheSourceFilesAfterMerge = true)
     {
         $instance = new self();
-        $instance->files = $files;
-        $instance->merge();
-        if (!$unlinkTheSourceFilesAfterMerge) {
-            return $instance;
-        }
-
-        foreach ($instance->files as $file) {
-            @unlink($file);
-        }
-        $instance->files = [];
-        return $instance;
-    }
-
-
-    /**
-     * PdfMerger constructor.
-     * @deprecated The public constructor is deprecated, please use the fromFilesIntoSinglePdf factory method
-     * @param string $orientation
-     * @param string $unit
-     * @param string $format
-     */
-    function __construct($orientation='P',$unit='mm',$format='A4') {
-        parent::__construct($orientation,$unit,$format);
-    }
-
-    /**
-     * @deprecated will be (re)moved  / privatised
-     * @param $files
-     */
-    function setFiles($files) {
-        $this->files = $files;
-    }
-
-    /**
-     * @deprecated will be (re)moved  / privatised
-     */
-    function merge() {
-        foreach ($this->files AS $file) {
-            $pagecount = $this->setSourceFile($file);
+        foreach ($files as $file) {
+            $pagecount = $instance->setSourceFile($file);
             for ($i = 1; $i <= $pagecount; $i++) {
-                 $tplidx = $this->ImportPage($i);
-                 $this->AddPage();
-                 $this->useTemplate($tplidx);
+                $tplidx = $instance->ImportPage($i);
+                $instance->AddPage();
+                $instance->useTemplate($tplidx);
             }
         }
+        if ($unlinkTheSourceFilesAfterMerge) {
+            foreach ($files as $file) {
+                @unlink($file);
+            }
+        }
+        return $instance;
     }
 
 }
